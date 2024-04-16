0d4c7394e7f909be9dbe7793729785fc15bb5d59
Amir Heydaripour <amir.h.heydaripour22@gmail.com>
2024-04-16 04:11:14 +0330

    First Commit for Added Files

---
+++ main-plugin.php
@@ -0,0 +1,17 @@
+<?php   
+/*
+ * Plugin Name:       Custom-test-plugin
+ * Plugin URI:        https://localhost/cli
+ * Description:       پلاگین تستی اول 
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Amir . H
+ * Author URI:        https://github.com/AmirheydaripourOdin1133
+ * License:           GPL v2 or later
+ * Text Domain:       پلاگین اختصاصی وردپرس - نسخه یک
+ * Domain Path:       /languages
+ */
+
+
+?>
